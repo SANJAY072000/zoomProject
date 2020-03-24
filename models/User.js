dea91bb7362d254e2515c723a98c06bb2fcb6b57
Sanjay Singh Bisht <sanjaysinghbisht114@gmail.com>
2020-03-24 19:13:15 +0530

    Done with schemas

---
+++ models/User.js
@@ -0,0 +1,13 @@
+// importing the required modules
+const mongoose=require('mongoose'),
+Schema=mongoose.Schema;
+
+
+
+
+
+// exporting the schema
+module.exports=mongoose.model('patient',patientSchema);
+
+
+
---
+++ routes/api/auth.js
@@ -0,0 +1,20 @@
+const express=require('express'),
+passport=require('passport'),
+router=express.Router(),
+bcrypt=require('bcryptjs'),
+jsonwt=require('jsonwebtoken'),
+nodemailer=require('nodemailer');
+
+
+
+
+
+
+
+
+
+
+// exporting the routes
+module.exports=router;
+
+
